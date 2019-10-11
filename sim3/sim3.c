8575298cf94d65091bc94c119a43d729ecc35d32
qclass <qclass@tutanota.com>
2019-10-10 21:46:07 -0700

    Project 3 sim3

--- sim3/sim3.c
+++ sim3/sim3.c
@@ -1,13 +1,13 @@
 /**
 * sim2.c
 *
-* Project 2
+* Project 3
 *
 * CS 646 Fall 2019
 * 
 * To compile, run makefile by entering "make"
 *
-* To run ./sim2 sim2.conf
+* To run ./sim3 sim3.conf
 *
 * Farhan Sadique
 */
@@ -25,18 +25,28 @@ typedef enum { false, true } bool;                // Typdef boolean
 bool log2mon = 1, log2file = 0;                   // Flag - log to monitor, file
 char log_fname[50];                               // log file name
 FILE *logfile;                                    // log file 
+
+unsigned int printsem = 1;
+unsigned int hddsem = 1;
+unsigned int curprint = 0;
+unsigned int curhdd = 0;
+
+unsigned int monmut = 1;
+unsigned int mousemut = 1;
+unsigned int keybmut = 1;
                                                   
+												  
 /* Structure / Enum definitions */                   
 struct Fin {char type[10], ext[10];};             // Used to validate config/meta file
 struct Io_d {                                     // Used to pass data to I/O thread
     int msec, proccount; char *code, *desc; 
 };                                                
 struct Config {                                   // Configuration 
-    int monitor, processor, mouse, hdd, keyboard, memory, printer, memkb;
+    int monitor, processor, mouse, hdd, keyboard, memory, printer, memkb, memblock, printer_q, hdd_q;
     char version[10], meta_fname[50], logmode[20], log_fname[50]; 
 };                                                
-enum pcb_s {START, READY, RUNNING, WAITING, EXIT};// pcb states
-struct PCB { enum pcb_s state;};                  // struct to store simulator pcb state
+
+struct Config conf = {0,0,0,0,0,0,0,0,0,1,1,"None","None","None","None"};
                                                   
                                                   
 // ============ Function Declarations ==========  
@@ -48,7 +58,7 @@ void *wait_t(void *arg);                          // entry point for I/O thread
 void io_thread(struct Io_d *io_ptr);              // create/join I/O thread, log and wait
 
 // ============= String Functions ==============
-int    _atoi(const char *str);                    // convert string to integer (own impelementation of atoi)
+int _atoi(const char *str);                       // convert string to integer (own impelementation of atoi)
 bool isspace(int c);                              // true if character c is whitespace
 bool isempty(const char *str);                    // true if string str is empty (whitespace)
 bool isinteger(const char *str);                  // true if string str is valid integer
@@ -69,14 +79,14 @@ void setlogmode(FILE *fconf);                     // sets universal log mode to 
 
 // ============= Configuration File Related Functions =============
 void startconf(FILE *fconf);                      // detect 'Start Simulator Configuration File', raise error otherwise
-int verify(char *val, const char *part);          // validate integer values for cycle ms and memory kb in config file
+int verify(char *val, const char *part, const char *msg); // validate integer values for cycle ms and memory kb in config file
 struct Config getconfig(FILE *fconf);             // get config data from config file in a struct to use later
                                                   
 // ============= Meta-Data File Related Function s ==============
 char *file2str(FILE *f);                          // reads entire file into string
 char *startmeta(char *buffer);                    // Detect 'Start Program Meta-Data Code:', raise error otherwise
 void codeVdesc(const char *token, const char *code, const char *desc);    // Validate meta-data code and descriptor
-int verifycycle(const char *code, const char *desc, const char *cycle);    // Validate meta-data cycle value is positive integer
+int verifycycle(const char *code, const char *desc, const char *cycle);   // Validate meta-data cycle value is positive integer
 void simulator(FILE *fmeta, struct Config conf);  // Main Simulator Function, simulates a meta-data file
 
 
@@ -109,51 +119,6 @@ int main(int argc, char* argv[]) {
     
     return 1;
 }
-
-
-
-// ========== Thead & Time Functions ==========
-
-double now() {return (double)clock()/CLOCKS_PER_SEC;}
-
-
-void wait(int msec){
-   // countdown timer in millisecond, sleep for msec
-    
-    clock_t before = clock(), diff;
-    double ms;
-    do {
-      diff = (double) (clock() - before);
-      ms = diff * 1000 / CLOCKS_PER_SEC;
-    } while ( ms < msec );
-}
-
-
-void *wait_t(void *arg){
-    // entry point for I/O thread
-    
-    struct Io_d io = *(struct Io_d*) arg;
-    int msec = io.msec, proccount = io.proccount;
-    char *code = io.code, *desc = io.desc, *temp;
-    
-    if (streq(code, "I")) temp = strdup("input");
-    else if (streq(code, "O")) temp = strdup("output");
-    
-    _log("%.6f - Process %d: start %s %s\n", now(), proccount, desc, temp);
-    wait(msec);
-    _log("%.6f - Process %d: end %s %s\n", now(), proccount, desc, temp);
-}
-
-
-void io_thread(struct Io_d *io_ptr){
-    // create/join I/O thread, log and wait
-    
-    pthread_t thread_id;
-    pthread_create(&thread_id, NULL, wait_t, io_ptr); 
-    pthread_join(thread_id, NULL); 
-    return;
-}
-
 
 
 // ============= String Functions =============
@@ -295,7 +260,6 @@ char *strsep(char **stringp, const char *delim) {
 }
 
 
-
 // ============== I/O Functions ==============
 
 int _log(const char *format, ...){
@@ -424,32 +388,25 @@ void startconf(FILE *fconf){
 }
 
 
-int verify(char *val, const char *part){
+int verify(char *val, const char *part, const char *msg){
     // validate cycle msec or memory kbytes as positive number
-    
-    char *msg;
     int m = 1;
+    char *unit=0;
     
-    char *unit = strsep(&val, "})");    // unit = msec for cycle, kbytes/Mbytes/Gbytes for memory
-    strsep(&val, ":");
-    strtrim(unit); strtrim(val);        // remove whitespace
-    
-    if (streq(part, "system memory")){
+    if ( !streq(msg, "quantity")) {
+        unit = strsep(&val, "})");      // unit = msec for cycle, kbytes/Mbytes/Gbytes for memory
+        strsep(&val, ":");
+        strtrim(unit); strtrim(val);    // remove whitespace
+    }
         
-        msg = strdup("size");
-        
+    if (streq(msg, "size")){       
         if (streq(unit, "kbytes")) ;
         else if (streq(unit, "mbytes")) m = 1024;
         else if (streq(unit, "gbytes")) m = 1024*1024;
         else logerror("\nError -- Invalid unit for '%s' %s: %s", part, msg, unit);
     } 
-    else {
-        if (streq(part, "monitor")) msg = strdup("display time");
-        else msg = strdup("cycle time");
-        
-        if ( !streq(unit, "msec")) 
-            logerror("\nError in config file --  Invalid unit for '%s' %s: %s", part, msg, unit);
-    }
+    else if (!streq(msg, "quantity") && !streq(unit, "msec"))
+        logerror("\nError in config file --  Invalid unit for '%s' %s: %s", part, msg, unit);
     
     if (isempty(val))                   // Missing cycle ms or memory kbytes
         logerror("\nError in config file -- Missing %s for '%s'\n", msg, part);
@@ -466,18 +423,16 @@ int verify(char *val, const char *part){
 
 
 struct Config getconfig(FILE *fconf){
-    // Gets config data into struct for future used
+    // Gets config data into struct for future use
     
     startconf(fconf);                   // Detect 'Start Simulator Configuration File' or raise error
     
     bool metafilegiven = 0;             // true if name of meta file given
     bool endconf = 0;                   // true if config file terminates normally
-    struct Config conf = {0,0,0,0,0,0,0,0,"None","None","None","None"};
     char line[256];                            
     
     while (fgets(line, sizeof(line), fconf)){
         strtrim(line);                  // Remove leading/trailing/multiple whitespace
-        //tolowerstr(line);               // Config file is case insensitive
         
         char *copy = &line[0];
         char *key = strsep(&copy, "{(:"); // key = before :
@@ -496,22 +451,27 @@ struct Config getconfig(FILE *fconf){
             endconf = 1; 
             break;
         } else if (isempty(val)){ logerror("\nError in config file -- missing value for %s", key);
-        } else if (streq(key, "monitor display time")){ conf.monitor = verify(val, "monitor");
-        } else if (streq(key, "processor cycle time")){ conf.processor = verify(val, "processor"); 
-        } else if (streq(key, "mouse cycle time")){     conf.mouse = verify(val, "mouse");
-        } else if (streq(key, "hard drive cycle time")){conf.hdd = verify(val, "hard drive");
-        } else if (streq(key, "keyboard cycle time")){  conf.keyboard = verify(val, "keyboard");
-        } else if (streq(key, "memory cycle time")){    conf.memory = verify(val, "memory");
-        } else if (streq(key, "printer cycle time")){   conf.printer = verify(val, "printer");
-        } else if (streq(key, "system memory")){        conf.memkb = verify(val, "system memory");
+        } else if (streq(key, "monitor display time")){ conf.monitor = verify(val, "monitor", "display time");
+        } else if (streq(key, "processor cycle time")){ conf.processor = verify(val, "processor", "cycle time"); 
+        } else if (streq(key, "mouse cycle time")){     conf.mouse = verify(val, "mouse", "cycle time");
+        } else if (streq(key, "hard drive cycle time")){conf.hdd = verify(val, "hard drive", "cycle time");
+        } else if (streq(key, "keyboard cycle time")){  conf.keyboard = verify(val, "keyboard", "cycle time");
+        } else if (streq(key, "memory cycle time")){    conf.memory = verify(val, "memory", "cycle time");
+        } else if (streq(key, "printer cycle time")){   conf.printer = verify(val, "printer", "cycle time");
+        } else if (streq(key, "system memory")){        conf.memkb = verify(val, "system memory", "size");
+        } else if (streq(key, "memory block size")){    conf.memblock = verify(val, "memory block", "size");
         } else if (streq(key, "version/phase")){        strcpy(conf.version, vcpy);
         } else if (streq(key, "log file path")){        strcpy(conf.log_fname, log_fname);
         } else if (streq(key, "file path")){            strcpy(conf.meta_fname, vcpy);
+		} else if (streq(key, "hard drive quantity")){  conf.hdd_q = verify(val, "hard drive", "quantity");
+                                                        hddsem = conf.hdd_q;
+		} else if (streq(key, "printer quantity")){     conf.printer_q = verify(val, "printer", "quantity");
+                                                        printsem = conf.printer_q;
         } else if (streq(key, "log")){
             if (log2mon && !log2file)      sprintf(conf.logmode, "%s", "monitor");
             else if (!log2mon && log2file) sprintf(conf.logmode, "%s", log_fname);
             else if (log2mon && log2file)  sprintf(conf.logmode, "monitor and %s", log_fname);
-        } else     logerror("\nError in config file -- Invalid config line: %s\n", line);
+        } else logerror("\nError in config file -- Invalid config line: %s\n", line);
     }
 
     if (streq(conf.meta_fname, "None")) // Meta file name not given
@@ -560,7 +520,7 @@ void codeVdesc(const char *token, const char *code, const char *desc){
     char P[][20]  = {"run"};
     char I[][20]  = {"hard drive", "keyboard", "mouse"};
     char O[][20]  = {"hard drive", "printer", "monitor"};
-    char M[][20]  = {"block", "allocate"};
+    char M[][20]  = {"block", "allocate", "cache"};
     
     char (*d)[20];
     if (*code == 'S' || *code == 'A') d = SA;
@@ -578,20 +538,19 @@ void codeVdesc(const char *token, const char *code, const char *desc){
 
 
 int verifycycle(const char *code, const char *desc, const char *cycle){
-    if (isempty(cycle) || *cycle == ' ') 
-        logerror("\nError -- missing cycle value for '%s' in: '%s{%s}%s'\n", desc, code, desc, cycle);
+    if (isempty(cycle) && !streq(desc, "begin") && !streq(desc, "finish") ) 
+        logerror("\nError in meta file -- missing cycle value for '%s' in: '%s{%s}%s'\n", desc, code, desc, cycle);
     
     if (!isinteger(cycle))
         logerror("\nError in meta file -- invalid (non-integer) cycles '%s' for '%s' in: '%s{%s}%s'\n", cycle, desc, code, desc, cycle);
     
     int c = _atoi(cycle);
     
-    if (c < 0)
-        logerror("\nError in meta file -- invalid (negative) cycles '%s' for '%s' in: '%s{%s}%s'\n", cycle, desc, code, desc, cycle);
-    
-    if (c == 0 && !streq(desc, "begin") && !streq(desc, "finish")) 
-        logerror("\nError -- invalid (zero) cycles '%s' for '%s' in: '%s{%s}%s'\n", cycle, desc, code, desc, cycle);
-    
+    if ( (streq(desc, "begin") || streq(desc, "finish")) && c != 0 )
+        logerror("\nError in meta file -- invalid (non-zero) cycles '%s' for '%s' in: '%s{%s}%s'\n", cycle, desc, code, desc, cycle);
+    else if (c <= 0 && !streq(desc, "begin") && !streq(desc, "finish"))
+        logerror("\nError in meta file -- invalid (non-positive) cycles '%s' for '%s' in: '%s{%s}%s'\n", cycle, desc, code, desc, cycle);
+
     return c;        
 }
 
@@ -609,6 +568,7 @@ int getmstot(int c, const char *desc, struct Config conf){
     else if (streq(desc, "allocate")) mspc = conf.memory;
     else if (streq(desc, "printer"))  mspc = conf.printer;
     else if (streq(desc, "block"))    mspc = conf.memory;
+    else if (streq(desc, "cache"))    mspc = conf.memory;
     else mspc = 0;
     
     if (mspc == 0 && !streq(desc, "begin") && !streq(desc, "finish"))
@@ -617,10 +577,135 @@ int getmstot(int c, const char *desc, struct Config conf){
     return c*mspc;    // Total time for operation
 }
 
+
+// ================= PCB Ready Queue =========================
+enum pcb_s {START, READY, RUNNING, WAITING, EXIT};// pcb states
+
+struct PCB {int id, burstms; enum pcb_s state;};    // struct to store simulator pcb state
+
+#define MAX 30
+
+struct PCB readyq[MAX];
+int front = 0;
+int rear = -1;
+int inq = 0;
+
+struct PCB peek() { return readyq[front]; }
+
+bool isEmpty() { return inq == 0; }
+
+bool isFull() { return inq == MAX; }
+
+int size() { return inq; }  
+
+void put(struct PCB data) {
+   if(!isFull()) {
+	
+      if(rear == MAX-1) {rear = -1;}       
+
+      readyq[++rear] = data;
+      inq++;
+   }
+}
+
+struct PCB get() {
+   struct PCB data = readyq[front++];
+	
+   if(front == MAX) {front = 0;}
+	
+   inq--;
+   return data;  
+}
+
+
+// ========== Thead & Time Functions ==========
+
+double now() {return (double)clock()/CLOCKS_PER_SEC;}
+
+
+void wait(int msec){
+   // countdown timer in millisecond, sleep for msec
+    
+    clock_t before = clock(), diff;
+    double ms;
+    do {
+      diff = (double) (clock() - before);
+      ms = diff * 1000 / CLOCKS_PER_SEC;
+    } while ( ms < msec );
+}
+
+
+void *wait_t(void *arg){
+    // entry point for I/O thread
+    
+    struct Io_d io = *(struct Io_d*) arg;
+    int msec = io.msec, proccount = io.proccount;
+    char *code = io.code, *desc = io.desc, *temp;
+    
+    if (streq(code, "I")) temp = strdup("input");
+    else if (streq(code, "O")) temp = strdup("output");
+    
+    char *part;
+    int *lock, *cur, curmax=-1;
+    if (streq(desc, "keyboard")) lock = &keybmut;
+    else if (streq(desc, "mouse")) lock = &mousemut;
+    else if (streq(desc, "monitor")) lock = &monmut;
+    else if (streq(desc, "hard drive")) {
+        lock = &hddsem;
+        cur = &curhdd;
+        curmax = conf.hdd_q - 1;
+        part = strdup("HDD");
+    } else if (streq(desc, "printer")) {
+        lock = &printsem;
+        cur = &curprint;
+        curmax = conf.printer_q - 1;
+        part = strdup("PRIN");
+    }
+    
+    int maxretry = 1000;
+    int retry = 0;
+    
+    while(1){
+        int unlocked = *lock;
+        if (unlocked) {
+            *lock = *lock-1;
+            break;
+        }
+        
+        wait(1);
+        
+        retry ++;
+        if (retry == maxretry)
+            logerror("\nError in simulation -- Cannot lock resource %s after %d retries\n", desc, maxretry);
+    }    
+    
+    if (curmax > -1) {
+        _log("%.6f - Process %d: start %s %s on %s %d\n", now(), proccount, desc, temp, part, *cur);
+        *cur = *cur + 1;
+        if (*cur > curmax) *cur = 0;
+    } else
+        _log("%.6f - Process %d: start %s %s\n", now(), proccount, desc, temp);
+   
+    wait(msec);
+    _log("%.6f - Process %d: end %s %s\n", now(), proccount, desc, temp);
+
+    *lock = *lock+1;
+}
+
+
+void io_thread(struct Io_d *io_ptr){
+    // create/join I/O thread, log and wait
+    
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, wait_t, io_ptr); 
+    pthread_join(thread_id, NULL); 
+    return;
+}
+
     
 void simulator(FILE *fmeta, struct Config conf){
     // Simulates meta-file
-    struct PCB pcbarr[10];
+    struct PCB pcbarr[20];
     
     char *buffer = file2str(fmeta);    // Read meta file into buffer string    
     buffer = startmeta(buffer);        // Detect Start Program Meta-Data Code: 
@@ -628,6 +713,8 @@ void simulator(FILE *fmeta, struct Config conf){
     char *code, *desc, *cycle;         // meta-data code, descriptor, cycles
     
     int proccount = 0;                 // process count - 1,2 ...
+    
+    unsigned int memblocktail = 0;
     
     while ((token = strsep(&buffer, ":;.")) != NULL) {
         strtrim(token);                                   // remove whitespace
@@ -692,11 +779,16 @@ void simulator(FILE *fmeta, struct Config conf){
                 _log("%.6f - Process %d: end memory blocking\n", now(), proccount);
             } else if (streq(desc, "allocate")){
                 if (conf.memkb == 0) logerror("Error -- system memory size not given in config file");
-                srand(time(NULL));
-                unsigned int loc = rand();
+                if (conf.memblock == 0) logerror("Error -- memory block size not given in config file");
+                
                 _log("%.6f - Process %d: allocating memory\n", now(), proccount);
+                
+                unsigned int loc = conf.memblock*memblocktail++;
+                if (memblocktail >= conf.memkb / conf.memblock)
+                    memblocktail = 0;
+                
                 wait(mstot);
-                _log("%.6f - Process %d: memory allocated at 0x%x\n", now(), proccount, loc);
+                _log("%.6f - Process %d: memory allocated at 0x%08X\n", now(), proccount, loc);
             }
         }
         
---
+++ submission/temp/ReadMe.md
@@ -0,0 +1,27 @@
+# Project 2 - sim2
+
+#### CS 646 Principles of Computer Operating Systems
+
+#### University of Nevada, Reno
+
+#### Farhan Sadique
+
+
+## Make and run
+
+```
+make
+./sim2 sim2.conf
+```
+
+#### Process Control Block (PCB)
+Line 38 - enum pcb_s {START, READY, RUNNING, WAITING, EXIT} decalred
+Line 39 - PCB struct declared
+Line 621 - PCB pcbarr[10] - holds PCB of 10 processes
+Line 669 - pcb state = START
+Line 671 - pcb state = READY
+Line 674 - pcb state = EXIT
+Line 677 - pcb state = RUNNING
+Line 683 - pcb state = WAITING
+Line 685 - pcb state = RUNNING (after WAITING)
+
